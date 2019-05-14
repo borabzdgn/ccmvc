fb50d0c0331b93cbc33104a0d1ef9c33da6191d3
bora bozdogan <borabzdgnn@gmail.com>
2019-05-14 15:56:42 +0300

    core geliştirmeleri ,controller ve error

--- application/Core/Core.php
+++ application/Core/Core.php
@@ -10,21 +10,30 @@ class ccmvc {
 	protected $homeController = "HomeController"; 
 	protected $methodName     = "index";
 	protected $paramsArray    = [];
-	public    $configFile     = '../mvc.settings.php';
+	public    $configFile     = 'application/mvc.settings.php';
 	
 	public function __construct(){
 		$request = $this->getURL();
+		print_r($request);
 		$request = ucfirst($request[0]);
-		require_once $configFile;
+		require_once($this->configFile);
+		$HC = $this->homeController;
 		
 		if(file_exists(APPDIR . 'Controller' . "\{$request}.php")) {
-			
+			require_once APPDIR . 'Controller' . "\{$request}.php";
+			require_once APPDIR . 'Core' . "\Controlller.php";
+		}elseif(file_exists(APPDIR . 'Controller' . "\{$HC}.php")) {
+			require_once APPDIR . 'Controller' . $HC . '\.php';
+			require_once APPDIR . 'Core' . $HC ."\Controlller.php";
+		}else {
+			require_once APPDIR . '\Core\Error.php';
+			// exception yazılacak
 		}
 	}
 	
 
 	public function getURL() {
-		if(isset(strip_tags($_GET['uri']))){
+		if(isset($_GET['uri'])){
 			$requestParse = explode("/",strip_tags($_GET['uri']));
 			return $requestParse;
 		}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	
+	/*
+		CCMVC - 13.05.2019
+		Bora BOZDOGAN
+	*/
+require "application/Core/core.php";
+$start = new ccmvc();
+
+
