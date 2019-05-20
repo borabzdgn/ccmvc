bd53dcc0b29d180795aa33bbf3d53f1e9e9dac00
bora bozdogan <borabzdgnn@gmail.com>
2019-05-20 21:29:13 +0300

    pdo sınıfı yazılmaya başlandı

---
+++ application/Class/pdo.class.php
@@ -0,0 +1,23 @@
+﻿<?php
+/*	
+	Bora BOZDOGAN
+	20.05.2019
+*/
+class ccPDO {
+	public $host = "localhost";
+	public $user = "root";
+	public $pass = "";
+	public $dbs  = "ccPDO";
+	
+	public function __construct(){
+		if (!defined('PDO::ATTR_DRIVER_NAME')) {
+			die('pdo sunucuda kapalı');
+		}
+		
+		$dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbs . "";
+		$dbs = new PDO($dsn,$this->user,$this->pass);
+		
+	}
+  
+  
+}
